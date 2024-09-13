72d8339fcaa3561e4965fc148466a4ec4f55fd63
Thayna Bezerra <arraisthayna@gmail.com>
2024-09-13 13:26:23 -0300

    css and js settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,css,js}"],
+  content: ["./src/**/*.{html,css,js}", "./index.html"],
   theme: {
     extend: {
       colors: {
